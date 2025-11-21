15305c7edd6bc6389afc326ca8132250633adbd0
Olga Balashova <olga.shadrina@gmail.com>
2025-11-21 13:50:42 -0800

    Add files via upload

---
+++ Domain/OrderTypes.fs
@@ -0,0 +1,25 @@
+﻿namespace Domain
+
+open System
+
+type OrderLine = {
+    Product : ProductCode
+    Quantity : int
+}
+
+type UnvalidatedOrder = {
+    OrderId : Guid
+    CustomerEmail : string
+    Lines : (string * int) list
+}
+
+type ValidatedOrder = {
+    OrderId : OrderId
+    CustomerEmail : Email
+    Lines : OrderLine list
+}
+
+type PlacedOrder = {
+    Order : ValidatedOrder
+    PlacedAt : DateTime
+}
---
+++ Domain/ProductCode.fs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+
+type ProductCode =
+    | Widget of string
+    | Gizmo of string
+
+module ProductCode =
+    let create (s: string) =
+        if s.StartsWith("W") then Ok (Widget s)
+        elif s.StartsWith("G") then Ok (Gizmo s)
+        else Error "Invalid product code"
